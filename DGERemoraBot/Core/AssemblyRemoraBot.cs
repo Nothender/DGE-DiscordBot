ef34f7a0a24927f43775465935bee3e686659320
Someone-42 <tristn.lemarchand@gmail.com>
2021-11-28 09:32:34 +0100

    RemoraBot - project setup

---
+++ DGERemoraBot/Core/AssemblyRemoraBot.cs
@@ -0,0 +1,27 @@
+﻿using EnderEngine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DGE.Core
+{
+    public static class AssemblyRemora
+    {
+        public const string NAME = "DGE-Remora";
+        public const string VERSION = "0.0.0.0";
+
+        public static readonly DGEModule module = new DGEModule(NAME, VERSION, Init);
+
+        internal static Logger logger = new Logger("DGE-Remora");
+
+
+        /// <summary>
+        /// Init function for the assembly (DGEModule)
+        /// </summary>
+        private static void Init()
+        {
+
+        }
+
+    }
+}
