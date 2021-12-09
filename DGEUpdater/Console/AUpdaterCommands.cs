c29c8b682e115eec675f5625bf7b3bf5797d4fb3
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2021-12-09 16:51:15 +0100

    Modules name AU command fix

--- DGEUpdater/Console/AUpdaterCommands.cs
+++ DGEUpdater/Console/AUpdaterCommands.cs
@@ -29,7 +29,7 @@ namespace DGE.Console
                 else if (int.TryParse(a[0], out int i))
                     UpdaterProgram.FetchVersions(i);
                 else if (Program.ProjectInfos.projectInfos.Any(p => p.Version.name.ToLower() == a[0]))
-                    UpdaterProgram.FetchVersions(Array.FindIndex(Program.ProjectInfos.projectInfos, p => p.Version.name == a[0]));
+                    UpdaterProgram.FetchVersions(Array.FindIndex(Program.ProjectInfos.projectInfos, p => p.Version.name.ToLower() == a[0]));
                 else
                     throw new ArgumentException("The argument does not fit any use");
 
