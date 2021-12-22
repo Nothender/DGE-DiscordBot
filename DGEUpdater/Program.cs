60e4d038680ad4e0b6bbbba625ee1b370c773ea4
Someone-42 <tristn.lemarchand@gmail.com>
2021-12-22 11:55:16 +0100

    Removed pile of junk

--- DGEUpdater/Program.cs
+++ DGEUpdater/Program.cs
@@ -51,61 +51,6 @@ namespace DGE
                 UpdaterLogging.WriteToMain($"Discovered {ProjectInfos.projectInfos.Length} project(s) :\n - {string.Join<ProjectInfo>("\n - ", ProjectInfos.projectInfos)}", Logger.LogLevel.INFO);
 
                 return;
-                foreach (ProjectInfo info in ProjectInfos.projectInfos) //TODO: way to enumerate over ProjectInfos (IProjectInfo to create) for cleaner code instead of keeping track of the index for the version and url
-                {
-                    try
-                    {
-                        char sep = '>';
-
-                        FetcherCollection.InitFetcher(info.FetcherOptions);
-
-                        IVersion latestVersion = DGEVersion.FromString(FetcherCollection.FetchLatestVersion(info.VersionLatestGet)); //Latest version from website
-                        IVersion localVersion = info.Version; //Local version calculated
-                        UpdaterLogging.WriteToMain($"\nProject {info.Version.name} :\n -> Latest version from website : {latestVersion}\n -> Local version calculated : {localVersion}", Logger.LogLevel.INFO);
-
-                        if (true || latestVersion.IsNewer(localVersion)) //If the version on the internet is newer than the local version
-                        //We try to download it or ask for download
-                        {
-
-                            try
-                            {
-                                UpdaterLogging.WriteToMain("Update available, Attempting download", Logger.LogLevel.INFO);
-                                System.Console.WriteLine(UpdaterTags.PassthroughInfo + UpdaterTags.UpdateAvailableTag);
-
-                                //FetcherCollection.DownloadLatestVersion(info.ProjectDlLatest[i].Split(sep));
-
-                                //TODO: Move down, so it extracts the files, once every project is done downloading | Extract with priorities : the first project downloaded should overwrite others if they share files
-
-                                //Download and app shutdown was successful : Extracting zip
-
-                                //System.Console.WriteLine(UpdaterTags.PassthroughInfo + UpdaterTags.UpdateDownloadedTag);
-                                UpdaterLogging.WriteToMain("Update downloaded succesfully", Logger.LogLevel.INFO);
-
-                                //TODO: Fetch latest release, and put it in a seperate directory
-                                //TODO: If download successful, shutdown launching process, move files to process folder, rerun launching process
-
-
-                                string file = Directory.GetFiles(Paths.Get("Downloads"))[0];
-                                if (file.EndsWith(".zip"))
-                                    ZipFile.ExtractToDirectory(file, Paths.Get("Contents"), true);
-
-                            }
-                            catch (Exception e)
-                            {
-                                UpdaterLogging.WriteToMain($"Couldn't download or install latest version :\n{e.Message}", Logger.LogLevel.ERROR);
-                            }
-                            //TODO: Try to download update/Ask if want to download
-                            //If downloading new update -> shutdown application that ran the updater.
-                        }
-                        else UpdaterLogging.WriteToMain("No update required", Logger.LogLevel.INFO);
-                    }
-                    catch (Exception e)
-                    {
-                        UpdaterLogging.WriteToMain($"Couldn't fetch the latest version of your project, maybe check if your repository is public (autoupdater doens't support logins yet) :\n{e.Message}", Logger.LogLevel.ERROR);
-                    }
-                }
-
-
             }
             catch (Exception e)
             {
