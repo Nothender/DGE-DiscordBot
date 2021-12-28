8644eeb281dbcf92e2205b73bc9651bca5525849
Someone-42 <tristn.lemarchand@gmail.com>
2021-12-28 11:56:12 +0100

    Linux script implementations

--- DGEFramework/Core/OperatingSystem/Scripts.cs
+++ DGEFramework/Core/OperatingSystem/Scripts.cs
@@ -7,19 +7,22 @@ namespace DGE.Core.OperatingSystem
     public static class Scripts
     {
         /// <summary>
-        /// Runs the app, you have to specify the FULL FilePath to the application you want to run in the createprocess args
+        /// Runs the app, you have to specify the FULL FilePath to the application you want to run in the createprocess args (has to be .NET)
         /// </summary>
         public static readonly Script RunApp = new Script("RunApp")
-            .DefineImplementation(OSPlatform.WINDOWS, "set app=%1\nstart %app%") as Script;
+            .DefineImplementation(OSPlatform.WINDOWS, "set app=%1\nstart %app%")
+            .DefineImplementation(OSPlatform.UNIX, "app=$1\ndotnet $app") as Script;
         /// <summary>
-        /// Moves the contents of "Contents" folder, then restarts the app passed in arguments, NEEDS DGE to shutdown
+        /// Moves the contents of "Contents" folder, then restarts the app (has to be .NET) passed in arguments, NEEDS DGE to shutdown
         /// </summary>
         public static readonly Script UpdateRestartApp = new Script("UpdateRestartApp")
-            .DefineImplementation(OSPlatform.WINDOWS, "set app=%1\nmove .\\Updater\\Contents\\*.* .\\\nstart %app%") as Script;
+            .DefineImplementation(OSPlatform.WINDOWS, "set app=%1\nmove .\\Updater\\Contents\\*.* .\\\nstart %app%")
+            .DefineImplementation(OSPlatform.UNIX, "app=$1\nmv ./Updater/Contents/* ./\ndotnet $app") as Script;
         /// <summary>
         /// Moves the contents of "Contents" folder, NEEDS DGE to shutdown
         /// </summary>
         public static readonly Script UpdateApp = new Script("UpdateApp")
-            .DefineImplementation(OSPlatform.WINDOWS, "move .\\Updater\\Contents\\*.* .\\") as Script;
+            .DefineImplementation(OSPlatform.WINDOWS, "move .\\Updater\\Contents\\*.* .\\")
+            .DefineImplementation(OSPlatform.UNIX, "mv ./Updater/Contents/* ./") as Script;
     }
 }
