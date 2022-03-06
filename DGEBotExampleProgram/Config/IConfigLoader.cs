668ba8d2fcb87ae81ea823e3cbd679fdd399b0e5
Someone-42 <tristn.lemarchand@gmail.com>
2022-03-06 13:24:02 +0100

    IConfig

---
+++ DGEBotExampleProgram/Config/IConfigLoader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DGE.Config
+{
+    public interface IConfigLoader
+    {
+        public IConfig LoadConfig();
+
+        public void SaveConfig(IConfig config);
+
+    }
+}
