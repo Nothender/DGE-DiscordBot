d6808b72fb937c7bc39e36f1c5c77bb388d0acfb
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-23 19:26:02 +0100

    Command configs for easier module loading

---
+++ DGEBot/Discord/Config/CommandConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGE.Discord.Config
+{
+
+    public class ModuleConfig : IModuleConfig
+    {
+
+        public string AssemblyQualifiedName { get; }
+
+        public string ModuleName { get; }
+
+        public ModuleConfig(string assemblyQualifiedName, string moduleName)
+        {
+            AssemblyQualifiedName = assemblyQualifiedName;
+            ModuleName = moduleName;
+        }
+    }
+
+    public class CommandConfig : ICommandConfig
+    {
+        public IModuleConfig[] modules { get; }
+
+        public CommandConfig(ModuleConfig[] modules) { this.modules = modules; }
+
+    }
+}
---
+++ DGEBot/Discord/Config/ICommandConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DGE.Discord.Config
+{
+
+    public interface IModuleConfig
+    {
+        /// <summary>
+        /// The assembly reference name, the module manager will try to find it from this
+        /// </summary>
+        string AssemblyQualifiedName { get; }
+
+        /// <summary>
+        /// Simpler name for readability
+        /// </summary>
+        string ModuleName { get; }
+    }
+
+    public interface ICommandConfig
+    {
+        /// <summary>
+        /// Represents the modules the bot will load/register
+        /// </summary>
+        IModuleConfig[] modules { get; }
+
+    }
+}
