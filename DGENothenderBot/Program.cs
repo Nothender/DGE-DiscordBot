5502b593ca6394536938ee64429ef0c9753cc5c0
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2023-03-30 21:08:18 +0200

    Program updated to new version stuff

--- DGENothenderBot/Program.cs
+++ DGENothenderBot/Program.cs
@@ -6,6 +6,7 @@ using DGE.Discord;
 using Discord;
 using Discord.WebSocket;
 using EnderEngine;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -22,6 +23,11 @@ namespace DGE
 
         private static void DiscordBotMain()
         {
+
+            DGE.Main.Init();
+            DGEModules.RegisterModule(AssemblyBot.module);
+            DGEModules.RegisterModule(AssemblyEngine.module);
+
             CreateBot();
 
             Task main = DGE.Main.Run();
@@ -36,9 +42,9 @@ namespace DGE
         {
             //TODO: This will be fixed to be cleaner
 #if DEBUG
-            string configFile = "config.txt"; //Running experimental config
+            string configFile = "configDebug.xml"; //Running experimental config
 #else
-            string configFile = "configDebug.txt"; //Running normal DGE config
+            string configFile = "config.xml"; //Running normal DGE config
 #endif
 
             IBotConfig config = LoadBotConfig(configFile);
@@ -49,20 +55,10 @@ namespace DGE
                 GatewayIntents = GatewayIntents.All ^ GatewayIntents.GuildPresences ^ GatewayIntents.GuildInvites ^ GatewayIntents.GuildScheduledEvents
             };
 
-            DGE.Main.Init();
-            DGEModules.RegisterModule(AssemblyBot.module);
-            DGEModules.RegisterModule(AssemblyEngine.module);
-
             DiscordBot bot1 = new DiscordBot("DGEExample");
             bot1.Load(config, socketConfig);
             ApplicationManager.Add(bot1);
 
-            bot1.client.InteractionCreated += async interaction =>
-            {
-                DGEInteractionContext ctx = new DGEInteractionContext(bot1, interaction);
-                System.Console.WriteLine("SUUU");
-                await bot1.interactionService.ExecuteCommandAsync(ctx, bot1.services);
-            };
 
             //bot1.LoadModuleConfig(new CommandConfig(new ModuleConfig[1] { new ModuleConfig("DGE.Discord.Commands.Commands,DGEBot", "BaseCommands") }));
 
@@ -89,19 +85,26 @@ namespace DGE
             //See config-exemple.txt for more information
             if (File.Exists(configFile))
             {
-                cfgLoader = new BotConfigXMLFileParser(configFile);
-                config = cfgLoader.LoadConfig();
+                try
+                {
+                    cfgLoader = new BotConfigXMLFileParser(configFile);
+                    config = cfgLoader.LoadConfig();
+                    return config;
+                }
+                catch (Exception e)
+                {
+                    logger.Log($"Exception occured whilst trying to load config from file : {e.Message}", Logger.LogLevel.ERROR);
+                }
             }
-            else
-            {
-                logger.Log("The program wasn't able to identify a valid config file\nAutomatically running console config file creation process", Logger.LogLevel.WARN);
-                cfgLoader = new ConsoleBotConfigLoader(logger);
-                config = cfgLoader.LoadConfig();
+            logger.Log("The program wasn't able to identify a valid config file\nAutomatically running console config file creation process", Logger.LogLevel.WARN);
 
-                IBotConfigSaver cfgSaver = new BotConfigXMLFileParser(configFile);
-                cfgSaver.SaveConfig(config);
-                logger.Log($"Saved newly created config (as '{configFile}')", Logger.LogLevel.INFO);
-            }
+            cfgLoader = new ConsoleBotConfigLoader(logger);
+            config = cfgLoader.LoadConfig();
+
+            IBotConfigSaver cfgSaver = new BotConfigXMLFileParser(configFile);
+            cfgSaver.SaveConfig(config);
+            logger.Log($"Saved newly created config (as '{configFile}')", Logger.LogLevel.INFO);
+
             return config;
         }
     }
